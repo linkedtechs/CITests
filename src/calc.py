054cbf80207aebd5ffadfb3875de02e75bb69c06
Sukhdeep <sdsingh.developer@gmail.com>
2019-11-27 02:58:55 +0100

    minor change

--- src/calc.py
+++ src/calc.py
@@ -2,9 +2,10 @@
 Calculator library containing basic math operations.
 """
 
+
 class Calculator:
-    def add(first_term, second_term):
+    def add(self, first_term, second_term):
         return first_term + second_term
 
-    def subtract(first_term, second_term):
+    def subtract(self, first_term, second_term):
         return first_term - second_term
--- test_calc.py
+++ test_calc.py
@@ -1,14 +1,13 @@
 """
 Unit tests for the calculator library
 """
-
-import calc
+import src.calc as Calculator
 
 
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == Calculator.add(2, 2)
+        assert 4 == Calculator().add(2, 2)
 
     def test_subtraction(self):
-        assert 2 == Calculator.subtract(4, 2)
+        assert 2 == Calculator().subtract(4, 2)
